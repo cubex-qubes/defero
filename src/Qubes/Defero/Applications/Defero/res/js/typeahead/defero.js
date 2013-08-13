49577bcbe8c26bc4b0fbba6643ba0c3f8fba05dd
Gareth Evans <gareth.evans@jdiuk.com>
2013-08-13 14:10:09 +0100

    Formatting

--- src/Qubes/Defero/Applications/Defero/res/js/typeahead/defero.js
+++ src/Qubes/Defero/Applications/Defero/res/js/typeahead/defero.js
@@ -1,30 +1,30 @@
 Defero.TypeAhead = {
-  const:     {
+  const:            {
     all:        "all",
     contacts:   "contacts",
     campaigns:  "campaigns",
     processors: "processors"
   },
-  all:       function (query, process) {
+  all:              function(query) {
     var th = Defero.TypeAhead;
     return th._deferoTypeAhead(th.const.all, query);
   },
-  contacts:  function (query, process) {
+  contacts:         function(query) {
     var th = Defero.TypeAhead;
     return th._deferoTypeAhead(th.const.contacts, query);
   },
-  campaigns: function (query, process) {
+  campaigns:        function(query) {
     var th = Defero.TypeAhead;
     return th._deferoTypeAhead(th.const.campaigns, query);
   },
-  processors: function (query, process) {
+  processors:       function(query) {
     var th = Defero.TypeAhead;
     return th._deferoTypeAhead(th.const.processors, query);
   },
   _deferoTypeAhead: function(type, query) {
     return (new jQuery.DeferoTypeAhead(type, query)).getResults();
   },
-  update: function(item) {
+  update:           function(item) {
     this.$element[0].value = item;
     this.$element[0].form.submit();
     return item;
