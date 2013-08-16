6866e7f73db8b0d0e6f63c51f88c0af1bf9fdfe1
Gareth Evans <gareth.evans@jdiuk.com>
2013-08-16 11:05:26 +0100

    Add a beradcrumb to show all steps (and current)

--- src/Qubes/Defero/Applications/Defero/Views/Wizard/StepsInfo.php
+++ src/Qubes/Defero/Applications/Defero/Views/Wizard/StepsInfo.php
@@ -9,9 +9,13 @@ use Cubex\View\HtmlElement;
 use Cubex\View\RenderGroup;
 use Cubex\View\ViewModel;
 use Qubes\Defero\Applications\Defero\Wizard\WizardStepIterator;
+use Qubes\Defero\Applications\Defero\Wizard\IWizardStep;
 
 class StepsInfo extends ViewModel
 {
+  /**
+   * @var \Qubes\Defero\Applications\Defero\Wizard\WizardStepIterator
+   */
   private $_steps;
 
   public function __construct(WizardStepIterator $steps)
@@ -21,7 +25,28 @@ class StepsInfo extends ViewModel
 
   public function render()
   {
+    $breadcrumb = new HtmlElement("ul", ["class" => "breadcrumb"]);
+
+    $breadcrumb->nestElement(
+      "li", [], new HtmlElement("span", ["class" => "divider"], "/")
+    );
+
+    foreach($this->_steps->getArrayCopy() as $stepKey => $step)
+    {
+      $active = $stepKey === $this->_steps->key() ? "active" : "";
+      
+      /**
+       * @var IWizardStep $step
+       */
+      $breadcrumb->nestElement(
+        "li",
+        ["class" => $active],
+        $step->getName() . new HtmlElement("span", ["class" => "divider"], "/")
+      );
+    }
+
     return new RenderGroup(
+      $breadcrumb,
       new HtmlElement("h3", [], $this->_steps->getCurrentStep()->getName()),
       new HtmlElement(
         "p", [], $this->_steps->getCurrentStep()->getDescription()
