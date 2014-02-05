08794942bf15861fa1778d264786674cf7981292
Tom Kay <tom.kay@justdevelop.it>
2014-02-05 11:00:53 +0000

    adjust height threshold

--- src/Qubes/Defero/Applications/Defero/res/js/statsgraph.js
+++ src/Qubes/Defero/Applications/Defero/res/js/statsgraph.js
@@ -10,9 +10,9 @@ jQuery(function() {
     maxFailedVal = parseInt(spark24h.data('max-failed'));
   spark24h
     .sparkline('html',{tagValuesAttribute:'data-queued', width:'350px', height:'65px',lineColor: 'blue', fillColor: false, chartRangeMax:maxQueuedVal,tooltipPrefix: 'Queued: '})
-    .sparkline('html',{composite: true, tagValuesAttribute:'data-test', lineColor: 'orange',fillColor: false,chartRangeMax:maxTestVal*1.5,tooltipPrefix: 'Test: '})
-    .sparkline('html',{composite: true, tagValuesAttribute:'data-sent', lineColor: 'green',fillColor: false,chartRangeMax:maxSentVal*1.5,tooltipPrefix: 'Sent: '})
-    .sparkline('html',{composite: true, tagValuesAttribute:'data-failed', lineColor: 'red',fillColor: false,chartRangeMax:maxFailedVal*3,tooltipPrefix: 'Failed: '});
+    .sparkline('html',{composite: true, tagValuesAttribute:'data-sent', lineColor: 'green',fillColor: false,chartRangeMax:maxSentVal*1.3,tooltipPrefix: 'Sent: '})
+    .sparkline('html',{composite: true, tagValuesAttribute:'data-test', lineColor: 'orange',fillColor: false,chartRangeMax:maxTestVal*2,tooltipPrefix: 'Test: '})
+    .sparkline('html',{composite: true, tagValuesAttribute:'data-failed', lineColor: 'red',fillColor: false,chartRangeMax:maxFailedVal*4,tooltipPrefix: 'Failed: '});
 
   var spark30d = jQuery('#spark30d');
   maxQueuedVal = parseInt(spark30d.data('max-queued'));
@@ -21,7 +21,7 @@ jQuery(function() {
   maxFailedVal = parseInt(spark30d.data('max-failed'));
   spark30d
     .sparkline('html',{tagValuesAttribute:'data-queued', width:'350px', height:'65px',lineColor: 'blue',fillColor: false,chartRangeMax:maxQueuedVal,tooltipPrefix: 'Queued: '})
-    .sparkline('html',{composite: true, tagValuesAttribute:'data-test', lineColor: 'orange',fillColor: false,chartRangeMax:maxTestVal*1.5,tooltipPrefix: 'Test: '})
-    .sparkline('html',{composite: true, tagValuesAttribute:'data-sent', lineColor: 'green',fillColor: false,chartRangeMax:maxSentVal*1.5,tooltipPrefix: 'Sent: '})
-    .sparkline('html',{composite: true, tagValuesAttribute:'data-failed', lineColor: 'red',fillColor: false,chartRangeMax:maxFailedVal*3,tooltipPrefix: 'Failed: '});
+    .sparkline('html',{composite: true, tagValuesAttribute:'data-sent', lineColor: 'green',fillColor: false,chartRangeMax:maxSentVal*1.3,tooltipPrefix: 'Sent: '})
+    .sparkline('html',{composite: true, tagValuesAttribute:'data-test', lineColor: 'orange',fillColor: false,chartRangeMax:maxTestVal*2,tooltipPrefix: 'Test: '})
+    .sparkline('html',{composite: true, tagValuesAttribute:'data-failed', lineColor: 'red',fillColor: false,chartRangeMax:maxFailedVal*4,tooltipPrefix: 'Failed: '});
 });
