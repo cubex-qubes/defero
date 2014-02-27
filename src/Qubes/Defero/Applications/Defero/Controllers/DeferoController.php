af58e9b2ed257f774975b62d96fe8382b8f50ecb
Brooke Bryan <brooke@bajb.net>
2014-02-27 14:55:39 +0000

    Queue stats on defero homepage

--- src/Qubes/Defero/Applications/Defero/Controllers/DeferoController.php
+++ src/Qubes/Defero/Applications/Defero/Controllers/DeferoController.php
@@ -5,13 +5,24 @@
 
 namespace Qubes\Defero\Applications\Defero\Controllers;
 
+use Cubex\Facade\Queue;
+use Cubex\Queue\Provider\Database\DatabaseQueue;
 use Qubes\Defero\Applications\Defero\Views\Index;
 
 class DeferoController extends BaseDeferoController
 {
   public function renderIndex()
   {
-    return new Index();
+    $index = new Index();
+    Queue::setDefaultQueueProvider('campaignqueue');
+
+    $queue = Queue::getAccessor();
+    if($queue instanceof DatabaseQueue)
+    {
+      $index->setQueueSize($queue->queueSize());
+      $index->setActiveQueueSize($queue->queueSize(1));
+    }
+    return $index;
   }
 
   public function getRoutes()
--- src/Qubes/Defero/Applications/Defero/Views/Index.php
+++ src/Qubes/Defero/Applications/Defero/Views/Index.php
@@ -5,12 +5,39 @@
 
 namespace Qubes\Defero\Applications\Defero\Views;
 
+use Cubex\View\HtmlElement;
+use Cubex\View\RenderGroup;
 use Cubex\View\ViewModel;
 
 class Index extends ViewModel
 {
+  protected $_queueSize = 0;
+  protected $_queueActiveSize = 0;
+
+  public function setQueueSize($size = 0)
+  {
+    $this->_queueSize = $size;
+    return $this;
+  }
+
+  public function setActiveQueueSize($size = 0)
+  {
+    $this->_queueActiveSize = $size;
+    return $this;
+  }
+
   public function render()
   {
-    return "Defero Index";
+    return new RenderGroup(
+      new HtmlElement('h1', [], 'Campaign Queue:'),
+      new HtmlElement(
+        'h2',
+        [],
+        number_format($this->_queueActiveSize, 0) .
+        '/' .
+        number_format($this->_queueSize, 0) .
+        ' Active'
+      )
+    );
   }
 }
